1ac17077f0818897f66284a3e61791406b1c971e
reus <reusee@gmail.com>
2021-11-27 20:31:38 +0800

    use config file

--- atproxy/main.go
+++ atproxy/main.go
@@ -6,32 +6,55 @@ import (
 	"os"
 	"path/filepath"
 
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/reusee/atproxy"
+	"github.com/reusee/starlarkutil"
+	"go.starlark.net/starlark"
 )
 
 func main() {
 
-	if len(os.Args) <= 1 {
-		pt("usage: %s [listen addr] [upstream addr]...\n",
-			filepath.Base(os.Args[0]),
-		)
-		return
-	}
-
-	addr := os.Args[1]
-	pt("listen: %s\n", addr)
-	ln, err := net.Listen("tcp", addr)
-	ce(err)
+	go func() {
+		ce(http.ListenAndServe("localhost:8000", nil))
+	}()
 
 	var options []atproxy.ServerOption
-	for _, arg := range os.Args[2:] {
-		pt("upstream: %s\n", arg)
-		options = append(options, atproxy.WithUpstream(atproxy.Upstream{
-			Addr: arg,
-		}))
-	}
 
 	options = append(options, atproxy.WithDenyDirectPattern("github"))
+	var socksAddr string
+
+	// load config file
+	exePath, err := os.Executable()
+	ce(err)
+	exeDir := filepath.Dir(exePath)
+	configFileName := "atproxy.py"
+	content, err := os.ReadFile(filepath.Join(exeDir, configFileName))
+	if err == nil {
+		pt("load %s\n", configFileName)
+		_, err := starlark.ExecFile(
+			new(starlark.Thread),
+			configFileName,
+			content,
+			starlark.StringDict{
+				"socks_addr": starlarkutil.MakeFunc("socks_addr", func(addr string) {
+					socksAddr = addr
+					pt("socks addr %s\n", addr)
+				}),
+				"upstream": starlarkutil.MakeFunc("upstream", func(addr string) {
+					options = append(options, atproxy.WithUpstream(atproxy.Upstream{
+						Addr: addr,
+					}))
+					pt("upstream %s\n", addr)
+				}),
+			},
+		)
+		ce(err)
+	}
+
+	ln, err := net.Listen("tcp", socksAddr)
+	ce(err)
 
 	server, err := atproxy.NewServer(
 		ln.(*net.TCPListener),
--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module github.com/reusee/atproxy
 
 go 1.17
 
-require github.com/reusee/e4 v0.0.0-20210929160631-f5ffcca999a5
+require (
+	github.com/reusee/e4 v0.0.0-20211021052625-556a58dc4948
+	github.com/reusee/starlarkutil v0.0.0-20211126070720-7790ee6f853a
+	go.starlark.net v0.0.0-20211013185944-b0039bd2cfe3
+)
 
-require github.com/hashicorp/golang-lru v0.5.4 // indirect
+require (
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/reusee/pr v0.0.0-20211003125556-3e6e9c7537ae // indirect
+	github.com/reusee/sb v0.0.0-20211013023636-c521ec7cac82 // indirect
+	golang.org/x/sys v0.0.0-20211020174200-9d6173849985 // indirect
+)
