03f4ad844234ecbe0ce99b08c8bbb9ebd5c67062
reus <reusee@gmail.com>
2021-11-27 21:04:17 +0800

    add atproxy.py example

---
+++ atproxy.py
@@ -0,0 +1,7 @@
+socks_addr("localhost:9103")
+http_addr("localhost:9102")
+upstream("100.118.120.94:1080")
+upstream("100.111.147.87:1080")
+upstream("100.73.249.57:1080")
+upstream("100.68.74.6:1080")
+
