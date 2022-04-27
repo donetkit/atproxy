1f37dccd3089621f84b0efcb842c8a3d4a41edc1
reus <reusee@gmail.com>
2022-04-27 09:21:51 +0800

    fix atproxy-exit.py

--- atproxy/atproxy-exit.py
+++ atproxy/atproxy-exit.py
@@ -2,8 +2,8 @@ pool_capacity(512)
 pool_buffer_size(4 * 1024)
 
 # for exit
-server(
-    tailscale_addr() + ":10000",
-    tailscale_addr() + ":10086",
+server_spec(
+    Socks = tailscale_addr() + ":10000",
+    HTTP = tailscale_addr() + ":10086",
 )
 
