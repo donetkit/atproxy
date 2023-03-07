115ff86ef41cc24c7002c3e155afcafc87ea263b
reus <reusee@gmail.com>
2023-03-08 00:36:48 +0800

    fix tailscale dialer

--- tailscale_linux.go
+++ tailscale_linux.go
@@ -2,17 +2,28 @@ package atproxy
 
 import (
 	"os"
+	"path/filepath"
 
 	"tailscale.com/tsnet"
 )
 
 func (Def) TailscaleDial() TailscaleDial {
+	exePath, err := os.Executable()
+	ce(err)
+	exeDir := filepath.Dir(exePath)
 	hostname, err := os.Hostname()
 	ce(err)
-	return (&tsnet.Server{
+	dir := filepath.Join(exeDir, "atproxy-tsnet")
+	ce(os.MkdirAll(dir, 0777))
+
+	server := &tsnet.Server{
+		Dir:      dir,
 		Hostname: hostname,
 		Logf: func(format string, args ...any) {
 			// do nothing
 		},
-	}).Dial
+	}
+	ce(server.Start())
+
+	return server.Dial
 }
