839622558fb357c647df402ef392e8f55e7d11e4
reus <reusee@gmail.com>
2023-03-31 02:00:16 +0800

    add *tsnet.Server def

--- tailscale_linux.go
+++ tailscale_linux.go
@@ -8,30 +8,29 @@ import (
 	"tailscale.com/tsnet"
 )
 
-var tsServer *tsnet.Server
+func (Def) TsServer() *tsnet.Server {
+	exePath, err := os.Executable()
+	ce(err)
+	exeDir := filepath.Dir(exePath)
+	hostname, err := os.Hostname()
+	ce(err)
+	dir := filepath.Join(exeDir, "atproxy-tsnet")
+	ce(os.MkdirAll(dir, 0777))
 
-var tsServerInitOnce sync.Once
+	tsServer = &tsnet.Server{
+		Dir:      dir,
+		Hostname: hostname,
+		Logf: func(format string, args ...any) {
+			// do nothing
+		},
+	}
+	ce(tsServer.Start())
 
-func (Def) TailscaleDial() TailscaleDial {
+	return tsServer
+}
 
-	tsServerInitOnce.Do(func() {
-		exePath, err := os.Executable()
-		ce(err)
-		exeDir := filepath.Dir(exePath)
-		hostname, err := os.Hostname()
-		ce(err)
-		dir := filepath.Join(exeDir, "atproxy-tsnet")
-		ce(os.MkdirAll(dir, 0777))
-
-		tsServer = &tsnet.Server{
-			Dir:      dir,
-			Hostname: hostname,
-			Logf: func(format string, args ...any) {
-				// do nothing
-			},
-		}
-		ce(tsServer.Start())
-	})
-
+func (Def) TailscaleDial(
+	tsServer *tsnet.Server,
+) TailscaleDial {
 	return tsServer.Dial
 }
